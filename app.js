fbcc0cf44c44cad36f529fc462fadaedcd4330ac
LohasOT <oliverhoang93@gmail.com>
2021-12-09 01:00:51 -0800

    added output and able to output file to new folder

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const fse = require('fs-extra');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -30,7 +31,7 @@ const add = () => {
     } else {
     const team = render(teamMembers)
     render(teamMembers)
-    fs.writeFile('team.html', team, err => {
+    fse.outputFile('output/team.html', team, err => {
         if (err) { console.log(err) }
       });
     }
